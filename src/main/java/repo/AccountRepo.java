1bd973caeda6ef93c62fb94fa1bf1a508a045ef5
jharry444 <jordan.harrison@qa.com>
2019-05-21 15:05:11 +0100

    Added gson

--- src/main/java/repo/AccountRepo.java
+++ src/main/java/repo/AccountRepo.java
@@ -12,5 +12,15 @@ public class AccountRepo {
 	public Map<Integer, Account> getAccounts() {
 		return accounts;
 	}
+	
+	private AccountRepo() {
+		
+	}
+	
+	private static AccountRepo repo = new AccountRepo();
+	
+	public static AccountRepo getInstance() {
+		return repo; 
+	}
 
 }
