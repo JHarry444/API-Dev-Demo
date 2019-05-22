c206afdad0a752fc226cb64b62f773a221316ef8
jharry444 <jordan.harrison@qa.com>
2019-05-22 14:31:46 +0100

    Made AccountRepoMap implement AccountRepo interface.

--- src/main/java/main/repo/AccountRepoMap.java
+++ src/main/java/main/repo/AccountRepoMap.java
@@ -3,23 +3,46 @@ package main.repo;
 import java.util.HashMap;
 import java.util.Map;
 
-import main.domain.Account;
+import javax.inject.Inject;
 
-public class AccountRepoMap {
+import main.domain.Account;
+import main.util.JSONUtil;
+
+public class AccountRepoMap implements AccountRepo {
 
 	private Map<Integer, Account> accounts = new HashMap<>();
 
-	public Map<Integer, Account> getAccounts() {
-		return accounts;
+	private int count;
+
+	@Inject
+	private JSONUtil json;
+
+	@Override
+	public String addAccount(String account) {
+		this.accounts.put(this.count++, this.json.fromJSON(account, Account.class));
+		return "{\"message\": \"account has been sucessfully added\"}";
 	}
-	private AccountRepoMap() {
-		
+
+	@Override
+	public Account getAccount(int id) {
+		return this.accounts.get(id);
 	}
-	
-	private static AccountRepoMap repo = new AccountRepoMap();
-	
-	public static AccountRepoMap getInstance() {
-		return repo; 
+
+	@Override
+	public String deleteAccount(int id) {
+		this.accounts.remove(id);
+		return "{\"message\": \"account sucessfully deleted\"}";
+	}
+
+	@Override
+	public Account updateAccount(int id, Account account) {
+		this.accounts.put(id, account);
+		return this.accounts.get(id);
+	}
+
+	@Override
+	public String getAllAccounts() {
+		return this.json.toJSON(this.accounts.values());
 	}
 
 }
--- src/main/java/main/rest/AccountEndpoint.java
+++ src/main/java/main/rest/AccountEndpoint.java
@@ -7,6 +7,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import main.service.AccountService;
 
@@ -29,7 +30,7 @@ public class AccountEndpoint {
 	
 	@Path("/getAccount/{id}")
 	@GET
-	@Produces({ "application/json" })
+	@Produces(MediaType.APPLICATION_JSON)
 	public String getAccount(@PathParam("id") Integer id) {
 		return service.getAccount(id);
 	}
